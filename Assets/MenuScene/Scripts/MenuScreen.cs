af536b0cc313c3a7c0f294f4983df2869116607c
Nikita <shpulinn@mail.ru>
2021-09-27 00:56:59 +0200

    Update MenuScreen.cs
    added changing language

--- Assets/MenuScene/Scripts/MenuScreen.cs
+++ Assets/MenuScene/Scripts/MenuScreen.cs
@@ -7,6 +7,27 @@ public class MenuScreen : MonoBehaviour {
     [SerializeField] private GameObject _mainMenuScreen;
     [SerializeField] private GameObject _optionsScreen;
 
+    private void Awake() {
+        var languageIndex = PlayerPrefs.GetInt("Language", 0);
+        switch (languageIndex) {
+            case 0:
+            Lean.Localization.LeanLocalization.SetCurrentLanguageAll("English");
+            break;
+            case 1:
+            Lean.Localization.LeanLocalization.SetCurrentLanguageAll("Russian");
+            break;
+            //case 2:
+            //Lean.Localization.LeanLocalization.SetCurrentLanguageAll("Chinese");
+            //break;
+            //case 3:
+            //Lean.Localization.LeanLocalization.SetCurrentLanguageAll("Arabic");
+            //break;
+            //case 4:
+            //Lean.Localization.LeanLocalization.SetCurrentLanguageAll("Spanish");
+            //break;
+        }
+    }
+
     public void PlayButtonClick() {
 
     }
